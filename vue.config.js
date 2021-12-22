d462c4b0c0cd2be436719395e0c936b5517e0b49
oliver6565 <390200763@qq.com>
2021-12-22 16:31:45 +0800

    更改百度地图api地址

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {  // 这里  你要去了解下 不然后面接口可能还会出现这个问题  只有知道原理了 才能正确的去处理问题
         '/api':  { 
-            target: 'http://api.map.baidu.com/geocoder?output=json&key=zSSQF0IkTliIBjcZlbNeNaDsWIHrCS0u&address=',
+            target: 'https://api.map.baidu.com/geocoder?output=json&key=zSSQF0IkTliIBjcZlbNeNaDsWIHrCS0u&address=',
             changeOrigin: true,
             withCredentials: true,
             pathRewrite: {
